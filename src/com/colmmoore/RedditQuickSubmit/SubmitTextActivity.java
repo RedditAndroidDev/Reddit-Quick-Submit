506f7ddec4046bbe7c654d190136fd81a364001a
nguyenmp <nguyenm605@yahoo.com>
2012-03-22 00:35:26 -0700

    fixed bug where the soft keyboard would cover the choose subreddit edit text and submit button.

--- src/com/colmmoore/RedditQuickSubmit/SubmitTextActivity.java
+++ src/com/colmmoore/RedditQuickSubmit/SubmitTextActivity.java
@@ -3,6 +3,7 @@ package com.colmmoore.RedditQuickSubmit;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class SubmitTextActivity extends Activity { 
     
@@ -10,20 +11,10 @@ public class SubmitTextActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);  	// Hides the horrible title at the top of the app. 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContentView(R.layout.submittext);
+        
         
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
