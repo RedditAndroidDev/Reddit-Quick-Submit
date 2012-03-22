5d4321e2278d868acbc38c598a8d1ee8d3eeaa9f
Frederick Maasch <member68@googlemail.com>
2012-03-22 08:59:18 +0100

    Removed unused imports

--- src/com/colmmoore/RedditQuickSubmit/RedditQuickSubmitActivity.java
+++ src/com/colmmoore/RedditQuickSubmit/RedditQuickSubmitActivity.java
@@ -1,18 +1,11 @@
 package com.colmmoore.RedditQuickSubmit;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
--- src/com/colmmoore/RedditQuickSubmit/SubmitImageActivity.java
+++ src/com/colmmoore/RedditQuickSubmit/SubmitImageActivity.java
@@ -1,16 +1,7 @@
 package com.colmmoore.RedditQuickSubmit;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-
 import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.Window;
 import android.widget.ImageView;
 
