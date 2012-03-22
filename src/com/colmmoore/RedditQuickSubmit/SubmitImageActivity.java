e258c5ea8be5183c70f359d8fef07dec58caf8bf
Colm Moore <mooreco4@gmail.com>
2012-03-22 01:00:57 +0000

    Added new Activity for submitting images

---
+++ src/com/colmmoore/RedditQuickSubmit/SubmitImageActivity.java
@@ -0,0 +1,27 @@
+package com.colmmoore.RedditQuickSubmit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubmitImageActivity extends Activity { 
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submittext);
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
