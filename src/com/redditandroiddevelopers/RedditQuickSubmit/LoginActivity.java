287442ec27966f661cab5481fce17c74aa6602b5
Colm Moore <mooreco4@gmail.com>
2012-03-25 23:43:42 +0100

    Added a spinner to the login function as the app came across as slow from the users point of view.

--- src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
@@ -8,16 +8,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.redditandroiddevelopers.RedditQuickSubmit.R;
 
 public class LoginActivity extends Activity {
-    
+
     private static LoginActivity instance;
-    
+
     // Start the main activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,18 +26,18 @@ public class LoginActivity extends Activity {
 	// Remove the title from the window; it doesn't look good.
 	requestWindowFeature(Window.FEATURE_NO_TITLE);
 	setContentView(R.layout.login);
-	
+
 	instance = this;
 
     };
-    
+
     public void onLoginClick(View v) {
-	
+
 	final ProgressDialog loginDialog;
-	
-	loginDialog = new ProgressDialog(LoginActivity.this);
-	loginDialog.setMessage("Loading, please wait!");
-	
+
+	loginDialog = new ProgressDialog(instance);
+	loginDialog.setMessage("Logging in, please wait!");
+
 	final EditText username = (EditText) findViewById(R.id.usernameForm);
 	final EditText password = (EditText) findViewById(R.id.passwordForm);
 
@@ -47,49 +46,62 @@ public class LoginActivity extends Activity {
 	    public void run() {
 
 		if (username.getText().toString().length() == 0) {
-		    
+
 		    instance.runOnUiThread(new Runnable() {
 			public void run() {
-			    Toast.makeText(instance, "Please enter a username!", Toast.LENGTH_SHORT).show();
+			    Toast.makeText(instance,
+				    "Please enter a username!",
+				    Toast.LENGTH_SHORT).show();
 			}
 		    });
 
 		} else if (password.getText().toString().length() == 0) {
-
+		    
 		    instance.runOnUiThread(new Runnable() {
 			public void run() {
-			    Toast.makeText(instance, "Please enter a password!", Toast.LENGTH_SHORT).show();
+			    Toast.makeText(instance,
+				    "Please enter a password!",
+				    Toast.LENGTH_SHORT).show();
 			}
 		    });
 
-
 		} else {
-			
-		    loginDialog.show();
+		    
+		    instance.runOnUiThread(new Runnable() {
+			public void run() {
+			    loginDialog.show();
+			}
+		    });
 
 		    final String url = "https://ssl.reddit.com/api/login/"
 			    + username.getText().toString();
 		    try {
 			LoginFunc func = new LoginFunc();
 			boolean loginSuccess = func.login(url, username
-				.getText().toString(), password
-				.getText().toString());
+				.getText().toString(), password.getText()
+				.toString());
+			
 			if (loginSuccess) {
 			    loginDialog.dismiss();
 			    // Login successful, carry on
 			    Intent myIntent = new Intent(instance,
-	    				RedditQuickSubmitActivity.class);
-			    startActivityForResult(myIntent, 0);    
+				    RedditQuickSubmitActivity.class);
+			    startActivityForResult(myIntent, 0);
+			    
 			} else {
+			    
 			    loginDialog.dismiss();
 			    instance.runOnUiThread(new Runnable() {
-				public void run() {				    
-				    Toast.makeText(instance, 
-					    "Error: " + LoginFunc.getErrorMessage(), 
-					    Toast.LENGTH_SHORT
-					    ).show();
+				public void run() {
+				    Toast.makeText(
+					    instance,
+					    "Error: "
+						    + LoginFunc
+							    .getErrorMessage(),
+					    Toast.LENGTH_SHORT).show();
 				}
-			    });			    
+			    });
+			    
 			}
 
 		    } catch (IOException e) {
@@ -99,9 +111,9 @@ public class LoginActivity extends Activity {
 	    }
 	}).start();
     }
-    
+
     public static Context getContext() {
-        return instance;
+	return instance;
     }
 
 }
