a0e419dbef69a58b55c921b1729ac55f9848bf47
Colm Moore <mooreco4@gmail.com>
2012-03-25 15:45:40 +0100

    Login function wasn't working when I clicked the button, so I changed the way you handled the button to a setOnClickListener

--- src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
@@ -2,6 +2,7 @@ package com.redditandroiddevelopers.RedditQuickSubmit;
 
 import java.io.IOException;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,19 +20,21 @@ public class LoginActivity extends Activity {
     // Start the main activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		// Remove the title from the window; it doesn't look good. 
-		requestWindowFeature(Window.FEATURE_NO_TITLE); 
-		setContentView(R.layout.login);
-    }
-    
-    public void submitLoginButton_OnClick(View v) {
-		
-		// Get the EditTexts from the XML
-		final EditText username = (EditText) findViewById(R.id.usernameForm);
-		final EditText password = (EditText) findViewById(R.id.passwordForm);
-		
+	super.onCreate(savedInstanceState);
+            		
+	// Remove the title from the window; it doesn't look good. 
+	requestWindowFeature(Window.FEATURE_NO_TITLE); 
+	setContentView(R.layout.login);
+	
+	final Button submitLoginButton = (Button) findViewById(R.id.submitLoginButton);
+	final EditText username = (EditText) findViewById(R.id.usernameForm);
+	final EditText password = (EditText) findViewById(R.id.passwordForm);
+	
+	
+	
+	submitLoginButton.setOnClickListener(new OnClickListener() { 				     
+	    @Override
+	    public void onClick(View v) { 
 		if (username.getText().toString().length() == 0){
 		    
 		    Context context = getApplicationContext();
@@ -62,16 +65,18 @@ public class LoginActivity extends Activity {
 		    	}
 		    	else {
 		    		Context context = getApplicationContext();
-				    CharSequence text = "Error: " + func.getErrorMessage();
+				    CharSequence text = "Error: " + func.getErrorMessage(); 
 				    int duration = Toast.LENGTH_SHORT;
 				    Toast toast = Toast.makeText(context, text, duration);
 				    toast.show();
 		    	}
 		    	
-	        } catch (IOException e) {
-	        	e.printStackTrace();
-	        }
+		    } catch (IOException e) {
+			e.printStackTrace();
+		    }
 		}
-	}
+	    }
+	});    		   		
+    }
 }
 
