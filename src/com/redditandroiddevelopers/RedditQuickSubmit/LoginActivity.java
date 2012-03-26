272f9d0c5bc2709627819c696a6434745fd63c36
Rheza Pahlevi <damniatx@gmail.com>
2012-03-27 01:32:29 +0800

    remove loginFunction, add sharedPreferences to save mod hash and cookie.

--- src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/LoginActivity.java
@@ -1,123 +1,243 @@
 
 package com.redditandroiddevelopers.RedditQuickSubmit;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.apache.http.impl.cookie.BasicClientCookie;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.PersistentCookieStore;
+
 
 public class LoginActivity extends Activity {
 
-    private static LoginActivity instance;
-
+  
+    
+   
+    
+    private static String errorType;
+    private static String errorMessage;
+    private static String errorCode;
+    private String modhash;
+    private String cookie;
+    SharedPreferences settings;
     // Start the main activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
+        
+        settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         // Remove the title from the window; it doesn't look good.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login);
 
-        PersistentCookieStore myCookieStore = new PersistentCookieStore(this);
-        myCookieStore.getCookies();
+        final Button Btn = (Button) findViewById(R.id.submitLoginButton);
+        
+        Btn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v)  {            
+            	
+            	
 
-        instance = this;
+              
 
+                 final EditText username = (EditText) findViewById(R.id.usernameForm);
+                 final EditText password = (EditText) findViewById(R.id.passwordForm);
+                 new loginRun().execute();
+            }
+        });
     };
 
-    public void onLoginClick(View v) {
+    private class loginRun extends AsyncTask<Void, Void, Void> 
+    {
+    	 
+        @Override
+        protected Void doInBackground(Void... unused) {
+        	 
+            return null;
+        }
 
-        final ProgressDialog loginDialog;
+        @Override
+        protected void onPostExecute(Void unused) {
+        	final EditText username = (EditText) findViewById(R.id.usernameForm);
+            final EditText password = (EditText) findViewById(R.id.passwordForm);
+            
+       	 if (username.getText().toString().length() == 0) {
 
-        loginDialog = new ProgressDialog(instance);
-        loginDialog.setMessage("Logging in, please wait!");
+                
+                Toast.makeText(getApplicationContext(),
+                        "Please enter a username!",
+                        Toast.LENGTH_SHORT).show();
+      
 
-        final EditText username = (EditText) findViewById(R.id.usernameForm);
-        final EditText password = (EditText) findViewById(R.id.passwordForm);
+    } else if (password.getText().toString().length() == 0) {
 
-        new Thread(new Runnable() {
+       
+                Toast.makeText(getApplicationContext(),
+                        "Please enter a password!",
+                        Toast.LENGTH_SHORT).show();
+      
 
-            public void run() {
+    } else {
 
-                if (username.getText().toString().length() == 0) {
 
-                    instance.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(instance,
-                                    "Please enter a username!",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
 
-                } else if (password.getText().toString().length() == 0) {
+        final String url = "https://ssl.reddit.com/api/login/"
+                + username.getText().toString();
+        try {
+            
+            boolean loginSuccess = login(url, username
+                    .getText().toString(), password.getText()
+                    .toString());
 
-                    instance.runOnUiThread(new Runnable() {
-                        public void run() {
-                            Toast.makeText(instance,
-                                    "Please enter a password!",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
+            if (loginSuccess) {
+                
+                // Login successful, carry on
+                Intent myIntent = new Intent(getApplicationContext(),
+                        RedditQuickSubmitActivity.class);
+                startActivityForResult(myIntent, 0);
 
-                } else {
+            } else {
 
-                    instance.runOnUiThread(new Runnable() {
-                        public void run() {
-                            loginDialog.show();
-                        }
-                    });
+              
+           
+                        Toast.makeText(
+                        		getApplicationContext(),
+                                "Error: "
+                                        + getErrorMessage(),
+                                Toast.LENGTH_SHORT).show();
+                 
 
-                    final String url = "https://ssl.reddit.com/api/login/"
-                            + username.getText().toString();
-                    try {
-                        LoginFunc func = new LoginFunc();
-                        boolean loginSuccess = func.login(url, username
-                                .getText().toString(), password.getText()
-                                .toString());
-
-                        if (loginSuccess) {
-                            loginDialog.dismiss();
-                            // Login successful, carry on
-                            Intent myIntent = new Intent(instance,
-                                    RedditQuickSubmitActivity.class);
-                            startActivityForResult(myIntent, 0);
-
-                        } else {
-
-                            loginDialog.dismiss();
-                            instance.runOnUiThread(new Runnable() {
-                                public void run() {
-                                    Toast.makeText(
-                                            instance,
-                                            "Error: "
-                                                    + LoginFunc
-                                                            .getErrorMessage(),
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            });
-
-                        }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
-        }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static Context getContext() {
-        return instance;
+        }
     }
 
+    
+    public boolean login(String u, String user, String pw) throws IOException {
+
+        URL url = new URL(u);
+
+        System.out.println("Username: " + user + ", Password: " + pw);
+        String data = "api_type=json&user=" + user + "&passwd=" + pw;
+        
+        HttpURLConnection ycConnection = null;
+        ycConnection = (HttpURLConnection) url.openConnection();
+        ycConnection.setRequestMethod("POST");
+        ycConnection.setDoOutput(true);
+        ycConnection.setUseCaches(false);
+        ycConnection.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded; charset=UTF-8");
+        ycConnection.setRequestProperty("Content-Length", String.valueOf(data
+                .length()));
+
+        DataOutputStream wr = new DataOutputStream(ycConnection
+                .getOutputStream());
+        wr.writeBytes(data);
+        wr.flush();
+        wr.close();
+        InputStream is = ycConnection.getInputStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        String line;
+        String response = "";
+        while ((line = rd.readLine()) != null) {
+            response += line;
+            response += '\r';
+        }
+        for (Entry<String, List<String>> r : ycConnection.getHeaderFields()
+                .entrySet()) {
+            System.out.println(r.getKey() + ": " + r.getValue());
+        }
+        rd.close();
+        System.out.println(response.toString());
+
+        // TODO: Check this case on real hardware.
+        // Sometimes the connection returns an empty string on the emulator.
+        if (response == null || response == "") {
+            return false;
+        }
+
+        boolean login_success = false;
+        JsonParser parser = new JsonParser();
+        JsonObject object = parser.parse(response).getAsJsonObject();
+        JsonObject members = object.getAsJsonObject("json");
+
+        JsonArray error = (JsonArray) members.getAsJsonArray("errors");
+        if (error.size() > 0) {
+            JsonArray errorDetails = (JsonArray) error.get(0);
+            errorType = errorDetails.get(0).getAsString();
+            errorMessage = errorDetails.get(1).getAsString();
+            errorCode = errorDetails.get(2).getAsString();
+        }
+
+        JsonObject dataObject = members.getAsJsonObject("data");
+        if (dataObject == null)
+            return login_success;
+
+        modhash = dataObject.get("modhash").getAsString();
+        cookie = dataObject.get("cookie").getAsString();
+        
+        
+        
+        if (modhash != "")
+            login_success = true;
+
+        System.out.println(cookie);
+        
+        AsyncHttpClient myClient = new AsyncHttpClient();
+
+        PersistentCookieStore myCookieStore = new PersistentCookieStore(getApplicationContext());
+        myClient.setCookieStore(myCookieStore);
+
+        BasicClientCookie newCookie = new BasicClientCookie("RedditQuickSubmit", cookie);
+      
+        newCookie.setVersion(1);
+        newCookie.setDomain("reddit.com");
+        newCookie.setPath("/");
+        myCookieStore.addCookie(newCookie);
+        
+        //save cookie and modhash in sharedpreferences
+      SharedPreferences.Editor editor = settings.edit();
+	 editor.putString("modhash", modhash);
+	  editor.putString("cookie", cookie);
+	  editor.putString("cookieValue", newCookie.toString());
+	   editor.commit();
+        
+        return login_success;
+    }
+
+    public static String getErrorMessage() {
+        return errorMessage;
+    }
 }
--- src/com/redditandroiddevelopers/RedditQuickSubmit/SubmitTextActivity.java
+++ src/com/redditandroiddevelopers/RedditQuickSubmit/SubmitTextActivity.java
@@ -1,17 +1,150 @@
 
 package com.redditandroiddevelopers.RedditQuickSubmit;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map.Entry;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.PersistentCookieStore;
+
+
+import android.preference.PreferenceManager;
+
 
 public class SubmitTextActivity extends Activity {
 
-    @Override
+    private static final String TAG = null;
+    private String Cookie;
+    
+    //
+    SharedPreferences settings;
+    
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.submittext);
+        AsyncHttpClient myClient = new AsyncHttpClient();
+    	PersistentCookieStore myCookieStore = new PersistentCookieStore(this);
+    	
+    	settings = PreferenceManager.getDefaultSharedPreferences(this);
+    
+    	Cookie = myCookieStore.getCookies().toString();
+    
+    		
+        final Button Btn = (Button) findViewById(R.id.submitTextButton);
+        
+        Btn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {            
+            	new submitLink().execute();
+            }
+        });
+       
     }
 
+class submitLink extends AsyncTask<String, Void, SubmitTextActivity> {
+    
+	    protected SubmitTextActivity doInBackground(String... urls) {
+
+	    	String u;
+			URL url = null;
+			try {
+				url = new URL("http://www.reddit.com/api/submit");
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//
+		
+		    
+			
+			  Log.v(TAG, "OUR COOKIE IS " + Cookie);
+			final EditText titleText = (EditText) findViewById(R.id.titleForm);
+			final EditText bodyText= (EditText) findViewById(R.id.bodyForm);
+			final EditText subRedditText= (EditText) findViewById(R.id.subredditForm);
+			InputStream is;
+			
+			String modhash = settings.getString("modhash", "");
+			String cookie = settings.getString("cookie", "");
+			String cookieValue = settings.getString("cookieValue", "");
+			
+	        String data = "&title=" + titleText.getText().toString() +"&text=" + titleText.getText().toString() +
+	        				"&sr=" + subRedditText.getText().toString() +"&sr=" + "self" +"&uh="+ modhash;
+	        
+			
+			Log.v(TAG, "OUR DATA IS " + data);
+	        HttpURLConnection ycConnection = null;
+	        try {
+					ycConnection = (HttpURLConnection)url.openConnection();
+					ycConnection.setRequestMethod("POST");
+			        ycConnection.setDoOutput(true);
+			        ycConnection.setUseCaches(false);
+			        ycConnection.setRequestProperty("Cookie","reddit_session=" + cookie);			        
+			        ycConnection.setRequestProperty("Content-Type",
+			                "application/x-www-form-urlencoded; charset=UTF-8");
+			        ycConnection.setRequestProperty("Content-Length", String.valueOf(data
+			                .length()));
+			        ycConnection.setDoInput(true);
+			        ycConnection.setDoOutput(true);
+			        DataOutputStream wr = new DataOutputStream(ycConnection.getOutputStream());
+			        wr.writeBytes(data);
+			        wr.flush();
+			        wr.close();
+			       is = ycConnection.getInputStream();
+			       BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			        String line;
+			        String response = "";
+			        
+			        while ((line = rd.readLine()) != null) {
+			            response += line;
+			            response += '\r';
+			        }
+			        for (Entry<String, List<String>> r : ycConnection.getHeaderFields()
+			                .entrySet()) {
+			            System.out.println(r.getKey() + ": " + r.getValue());
+			        }
+			        rd.close();
+			        System.out.println(response.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	       
+
+	       
+	       
+		    return null;
+			
+			
+	    }
+
+
+		protected void onPostExecute(SubmitTextActivity feed) {
+			
+	    }
+	 }
+	 
+	
 }
